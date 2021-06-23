1fdc18e5302aa8c72f7e7a1a20671b192c94e818
maychaza <mayalejandrr@gmail.com>
2021-06-23 18:16:38 -0300

    se crean los primeros web methods incompletos

---
+++ src/main/java/ar/com/ada/api/empleadas/controllers/CategoriaController.java
@@ -0,0 +1,39 @@
+package ar.com.ada.api.empleadas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.ada.api.empleadas.entities.Categoria;
+import ar.com.ada.api.empleadas.models.response.GenericResponse;
+
+@RestController
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaService service;
+
+    // --post/ruta
+    @PostMapping("/categorias") //ningun web method devuelve void
+    public ResponseEntity<?> crearCategoria (@RequestBody Categoria categoria){
+        
+        GenericResponse respuesta = new GenericResponse();
+        service.crearCategoria(categoria);
+
+        respuesta.isOk= true;
+        respuesta.id= categoria.getCategoriaId();
+        respuesta.message= "La categoria fue creada con éxito";
+        
+        return ResponseEntity.ok(respuesta);
+    }
+
+    //--get/categorias
+    @GetMapping("/categorias") //hacer mapping
+    public ResponseEntity<List<Categoria>> traerCategorias(){ //return responseentity
+        return ResponseEntity.ok(service.traerCategorias()); //return entity con el valor esperado
+    }
+    
+}
--- src/main/java/ar/com/ada/api/empleadas/entities/Empleada.java
+++ src/main/java/ar/com/ada/api/empleadas/entities/Empleada.java
@@ -64,6 +64,7 @@ public class Empleada {
 
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
+        this.categoria.agregarEmpleada(this);
     }
 
     public BigDecimal getSueldo() {
