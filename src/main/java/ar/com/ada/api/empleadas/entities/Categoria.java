3bdaf090fe647f5b714abd75d418000c3e8513b6
maychaza <mayalejandrr@gmail.com>
2021-06-23 18:16:01 -0300

    se agregan los primeros web methods incompletos

--- src/main/java/ar/com/ada/api/empleadas/entities/Categoria.java
+++ src/main/java/ar/com/ada/api/empleadas/entities/Categoria.java
@@ -35,22 +35,38 @@ public class Categoria {
     public Integer getCategoriaId() {
         return categoriaId;
     }
+
     public void setCategoriaId(Integer categoriaId) {
         this.categoriaId = categoriaId;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public BigDecimal getSueldoBase() {
         return sueldoBase;
     }
+
     public void setSueldoBase(BigDecimal sueldoBase) {
         this.sueldoBase = sueldoBase;
     }
     
+    public List<Empleada> getEmpleadas() {
+        return empleadas;
+    }
+
+    public void setEmpleadas(List<Empleada> empleadas) {
+        this.empleadas = empleadas;
+    }
+
+    public void agregarEmpleada(Empleada empleada){
+        this.empleadas.add(empleada);
+    }
 }
 
 
---
+++ src/main/java/ar/com/ada/api/empleadas/services/CategoriaService.java
@@ -0,0 +1,9 @@
+package ar.com.ada.api.empleadas.services;
+
+public class CategoriaService {
+
+    private CategoriaRepository repository;
+
+    
+    
+}
