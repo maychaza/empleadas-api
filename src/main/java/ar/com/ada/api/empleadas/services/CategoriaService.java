5998793a3bfa8479f40779446095f7f2f9f6148c
maychaza <mayalejandrr@gmail.com>
2021-06-24 16:39:31 -0300

    Se terminan de crear el Service de categoria y el repode categoria con los metodos base

--- src/main/java/ar/com/ada/api/empleadas/services/CategoriaService.java
+++ src/main/java/ar/com/ada/api/empleadas/services/CategoriaService.java
@@ -1,9 +1,25 @@
 package ar.com.ada.api.empleadas.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.empleadas.entities.Categoria;
+import ar.com.ada.api.empleadas.repos.*;
+
+@Service
 public class CategoriaService {
 
+    @Autowired
     private CategoriaRepository repository;
 
-    
+    public void crearCategoria(Categoria categoria){
+        repository.save(categoria);
+    }
+
+    public List<Categoria> traerCategorias(){
+        return repository.findAll();
+    }
     
 }
